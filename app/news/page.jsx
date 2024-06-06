45f283ffebce67ec6fefb6d7aa1a7845a6ef6061
mzrar-myrtle <mzrar@myrtle.iq>
2024-06-06 14:41:28 +0300

    Feat(components\news-list.jsx): Adding NewsList component to showing list of news and fixing the archive paralel routes

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,25 +1,8 @@
-import Link from "next/link";
 import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/news-list";
 
 const NewsPage = () => {
-  return (
-    <>
-      <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((newsItem) => (
-          <li key={newsItem.id}>
-            <Link href={`/news/${newsItem.slug}`}>
-              <img
-                src={`/images/news/${newsItem.image}`}
-                alt={newsItem.title}
-              />
-              <span>{newsItem.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return <NewsList news={DUMMY_NEWS} />;
 };
 
 export default NewsPage;
---
+++ components/news-list.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { DUMMY_NEWS } from "@/dummy-news";
+
+const NewsList = ({ news }) => {
+  console.log("PASS: ", news);
+  return (
+    <ul className="news-list">
+      {news.map((newsItem) => (
+        <li key={newsItem.id}>
+          <Link href={`/news/${newsItem.slug}`}>
+            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+          </Link>
+          <span>{newsItem.title}</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NewsList;
