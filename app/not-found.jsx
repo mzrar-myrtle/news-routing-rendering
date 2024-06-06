f6e6554c10fd7c883a9ba6dc042fa55b654dc2e3
mzrar-myrtle <mzrar@myrtle.iq>
2024-06-06 12:59:24 +0300

    Feat(app\news\[slug]\not-found.jsx): Handling notfound page in news/[slug] route

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,8 +1,9 @@
 const NotFound = () => {
   return (
-    <>
-      <h1>Not Found</h1>
-    </>
+    <div id="error">
+      <h1>Not Found!</h1>
+      <p>The requested resource could not be found!</p>
+    </div>
   );
 };
 
