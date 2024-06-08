4dcea9612912b10c97ad6540092a67d2700192cb
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-06-08 14:02:44 +0300

    Feat(app\archive\@archive\[[...filter]]\error.jsx): Adding error for having not existed year or month in the route

---
+++ app/archive/@archive/[[...filter]]/error.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+const FilterError = () => {
+  return (
+    <div className="error">
+      <h2>An Error occurred!</h2>
+      <p>Invalid path.</p>
+    </div>
+  );
+};
+
+export default FilterError;
