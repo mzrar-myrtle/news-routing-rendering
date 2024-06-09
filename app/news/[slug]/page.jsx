006b80538b9235617f31b4baafae20a4e6bcb1b5
mzrar-myrtle <mzrar@myrtle.iq>
2024-06-09 09:38:21 +0300

    Feat(app\news\[slug]\image\page.jsx): Adding relative path

--- app/news/[slug]/page.jsx
+++ app/news/[slug]/page.jsx
@@ -1,5 +1,6 @@
 import { DUMMY_NEWS } from "@/dummy-news";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 const NewsDetailPage = ({ params }) => {
   const newsSlug = params.slug;
@@ -12,7 +13,9 @@ const NewsDetailPage = ({ params }) => {
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
       </header>
       <time dateTime={newsItem.date}>{newsItem.date}</time>
       <p>{newsItem.content}</p>
